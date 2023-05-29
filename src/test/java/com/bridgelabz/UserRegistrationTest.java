49d2688c4a42a529c8ca083977a9bdc4fb4b222e
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-30 01:02:35 +0530

    Added: Custom Exception

---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationTest {
+
+    @Test
+    public void givenNameShouldReturnCustomExceptionIfNameIsNotValid(){
+        System.out.println("Welcome to Day 21 assignment.");
+    }
+}
